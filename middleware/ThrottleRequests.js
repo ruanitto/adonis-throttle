edd936f2a78d2d98fc174ce4a1d8caa768f61d3e
Rafael Gomes <rafael.gomes@madsolutions.com.br>
2021-04-08 20:15:30 -0300

    Ajusts

--- middleware/ThrottleRequests.js
+++ middleware/ThrottleRequests.js
@@ -14,86 +14,86 @@ const TooManyRequestsException = require('../src/Exceptions/TooManyRequests')
 
 class ThrottleRequests {
 
-    constructor(throttle) {
-        this.throttle = throttle
+  constructor(throttle) {
+    this.throttle = throttle
+  }
+
+  /**
+   *
+   * Handle an incoming request.
+   *
+   * @param   {Request}     request
+   * @param   {Response}    response
+   * @param   {Function}    next
+   * @param   {Number}      maxAttempts     [optional, default = 60]
+   * @param   {Number}      decayInSeconds    [optional, default = 1]
+   * @param   {String}      uid
+   * @return  {Response|Function}
+   *
+   * @public
+   */
+  async handle({ request, response }, next, [maxAttempts = 60, decayInSeconds = 60], uid = false) {
+    const signature = this._resolveSignature(request, uid)
+    this.throttle.resource(signature, parseInt(maxAttempts), parseInt(decayInSeconds))
+
+    if (!await this.throttle.attempt()) {
+      await this.throttle.incrementExpiration()
+      this._addHeaders(
+        response,
+        maxAttempts,
+        await this.throttle.remainingAttempts(),
+        await this.throttle.store.secondsToExpiration(this.throttle.key)
+      )
+      throw new TooManyRequestsException('Too Many Attempts.')
     }
 
-    /**
-     *
-     * Handle an incoming request.
-     *
-     * @param   {Request}     request
-     * @param   {Response}    response
-     * @param   {Function}    next
-     * @param   {Number}      maxAttempts     [optional, default = 60]
-     * @param   {Number}      decayInSeconds    [optional, default = 1]
-     * @param   {String}      uid
-     * @return  {Response|Function}
-     *
-     * @public
-     */
-    async handle({ request, response }, next, [maxAttempts = 60, decayInSeconds = 60], uid = false) {
-        const signature = this._resolveSignature(request, uid)
-        this.throttle.resource(signature, parseInt(maxAttempts), parseInt(decayInSeconds))
+    this._addHeaders(
+      response,
+      maxAttempts,
+      await this.throttle.remainingAttempts()
+    )
 
-        if (!await this.throttle.attempt()) {
-            await this.throttle.incrementExpiration()
-            this._addHeaders(
-                response,
-                maxAttempts,
-                await this.throttle.remainingAttempts(),
-                await this.throttle.store.secondsToExpiration(this.throttle.key)
-            )
-            throw new TooManyRequestsException('Too Many Attempts.')
-        }
+    await next()
+  }
 
-        this._addHeaders(
-            response,
-            maxAttempts,
-            await this.throttle.remainingAttempts()
-        )
-
-        await next()
+  /**
+   * Resolve signature.
+   *
+   * @param   {Request}     request
+   * @param   {String}      uid
+   * @return  {String}
+   *
+   * @private
+   */
+  _resolveSignature(request, uid) {
+    let generator = Crypto.createHash('sha1')
+    if (uid === false) {
+      generator.update(`${request.method()}|${request.url()}|${request.ip()}`)
+    } else {
+      generator.update(uid)
     }
+    return generator.digest('hex')
+  }
 
-    /**
-     * Resolve signature.
-     *
-     * @param   {Request}     request
-     * @param   {String}      uid
-     * @return  {String}
-     *
-     * @private
-     */
-    _resolveSignature(request, uid) {
-        let generator = Crypto.createHash('sha1')
-        if (uid === false) {
-            generator.update(`${request.method()}|${request.url()}|${request.ip()}`)
-        } else {
-            generator.update(uid)
-        }
-        return generator.digest('hex')
+  /**
+   * Add the limit header information to the given response.
+   *
+   * @param   {Response}    response
+   * @param   {Number}      maxAttempts
+   * @param   {Number}      remainingAttempts
+   * @param   {Number}      retryAfter          [optional, default = null]
+   * @return  {void}
+   *
+   * @private
+   */
+  _addHeaders(response, maxAttempts, remainingAttempts, retryAfter = null) {
+    response.header('X-RateLimit-Limit', maxAttempts)
+    response.header('X-RateLimit-Remaining', remainingAttempts)
+    if (retryAfter !== null) {
+      response.header('Retry-After', retryAfter)
+      response.header('X-RateLimit-Reset', new Date().getTime() + (retryAfter * 1000))
     }
-
-    /**
-     * Add the limit header information to the given response.
-     *
-     * @param   {Response}    response
-     * @param   {Number}      maxAttempts
-     * @param   {Number}      remainingAttempts
-     * @param   {Number}      retryAfter          [optional, default = null]
-     * @return  {void}
-     *
-     * @private
-     */
-    _addHeaders(response, maxAttempts, remainingAttempts, retryAfter = null) {
-        response.header('X-RateLimit-Limit', maxAttempts)
-        response.header('X-RateLimit-Remaining', remainingAttempts)
-        if (retryAfter !== null) {
-            response.header('Retry-After', retryAfter)
-            response.header('X-RateLimit-Reset', new Date().getTime() + (retryAfter * 1000))
-        }
-    }
+  }
 
 }
 
